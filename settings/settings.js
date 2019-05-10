136adc6dffc9ed74027d813b01b06209a9830584
Fran Cardoso <fran.adalid@hotmail.com>
2019-05-10 18:45:07 -0300

    new allow origin

--- settings/settings.js
+++ settings/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 3002,
     SESSION_SECRET: process.env.SESSION_SECRET || 'L0La An/V3r',
-    CORS_ALLOWED_ORIGINS: process.env.CORS_ALLOWED_ORIGINS || ['http://localhost:8080', "https://teste-react.now.sh", "https://shalom.now.sh/"],
+    CORS_ALLOWED_ORIGINS: process.env.CORS_ALLOWED_ORIGINS || ['http://localhost:8080', "https://teste-react.now.sh", "https://shalom.now.sh"],
     DB_PATH: process.env.DB_PATH || 'mongodb://localhost:27017/shalomLocal',
 }
